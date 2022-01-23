0d9eb4355e3e67ed91fde425762b25fecf1ab8a4
idobetesh <idobetesh@gmail.com>
2022-01-23 20:20:35 +0200

    add hello confing script

---
+++ ubuntu-config/hello.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# This .sh file created in order to config a new ubuntu machine for DroneGuard Project
+# ======= cat /etc/*-release ========
+# PRETTY_NAME="Raspbian GNU/Linux 10 (buster)"
+# NAME="Raspbian GNU/Linux"
+# VERSION_ID="10"
+# VERSION="10 (buster)"
+# VERSION_CODENAME=buster
+# ID=raspbian
+# ID_LIKE=debian
+# HOME_URL="http://www.raspbian.org/"
+# SUPPORT_URL="http://www.raspbian.org/RaspbianForums"
+# BUG_REPORT_URL="http://www.raspbian.org/RaspbianBugs"
+ 
+echo === Update current ubuntu === 
+sudo apt install ntp;
+sudo apt clean; sudo apt update; sudo apt list --upgradable;
+sudo apt dist-upgrade;
+sudo apt autoremove;   
+
+echo === Set aliases ===
+echo alias l=\'ls -CF\'$'\n'alias ll=\'ls -alF\'$'\n'alias la=\'ls -A\' > ~/.bash_aliases;
+sleep 0.5;
+source ~/.bash_aliases;
+
+echo === Install git ===
+sudo apt -y install git;
+
+echo === Install Node and NPM ===
+sudo apt -y install nodejs;
+sudo apt -y install npm;
+
+echo === Install Python ===
+sudo apt-get -y install python3;
+echo alias python=python3 >> ~/.bash_aliases; sleep 0.5; source ~/.bash_aliases;
+
+echo === Set Local Time ===
+sudo dpkg-reconfigure tzdata;
