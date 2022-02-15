becfaacfea89e3b6eef635cf90efbe6adb3678de
idobetesh <idobetesh@gmail.com>
2022-02-15 17:19:56 +0200

    add user when exists to logs

--- server/api/middlewares/logger-middleware.js
+++ server/api/middlewares/logger-middleware.js
@@ -1,8 +1,11 @@
 const loggerMiddleware = (req, res, next) => {
     const startTime = new Date();
+    const userConnected = req.user ? true : false;
+
     const logData = {
         method: req.method,
-        url: req.url
+        url: req.url,
+        ...userConnected && { user: req.user.id }
     };
 
     console.log(`[logger ${startTime.toLocaleString()}]: ${JSON.stringify(logData)}`);
