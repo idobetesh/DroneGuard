22969dc6feaee91e4719a5131a729886028971ad
idobetesh <idobetesh@gmail.com>
2022-03-13 22:09:55 +0200

    update camera/server with correct routes

--- Hardware/Camera/server/cleaner.js
+++ Hardware/Camera/server/cleaner.js
@@ -1,6 +1,6 @@
-const findRemoveSync = require('find-remove')
+const findRemoveSync = require('find-remove');
 
 setInterval(() => {
-    const result = findRemoveSync('./videos/ipcam', { age: { seconds: 30 }, extensions: '.ts' });
+    const result = findRemoveSync('/home/pi/DroneGuard/Hardware/Camera/video/ipcam', { age: { seconds: 30 }, extensions: '.ts' });
     console.log(result);
 }, 5000);
