818917e4f4180883e21666cb403d1d493db69afe
idobetesh <idobetesh@gmail.com>
2022-05-03 23:59:27 +0300

    handle missing args in shell script and update readme

--- hardware/scripts/record-conversion.sh
+++ hardware/scripts/record-conversion.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# NOTE - LOCAL USE ONLY!
+
 # Prerequisites: jq (https://stedolan.github.io/jq/)
 
 # This script converts, uploads videos to S3 Bucket and creates record objects in DG DB
@@ -7,13 +9,19 @@
 
 # args[1] => url to S3 bucket [should ends with `/`]
 # args[2] => user email
-# args[2] => user password
+# args[3] => user password
 AWS_URL=$1 
 EMAIL=$2
 PASSWORD=$3
 
-BUCKET_NAME=drone-guard-debriefing
+BUCKET_NAME=mid-debriefing-bucket
 DIR_NAME=/Users/ido/Desktop/local-videos/recordings/
+
+if [ $# -lt 3 ]; then
+  echo 1>&2 "$0: One or more arguments are missing"
+  echo '[$./record-conversion.sh <BUCKET_URL> <USER_EMAIL> <USER_PASSWORD>]'
+  exit 0
+fi
 
 # Make sure to have wifi connection
 echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
@@ -25,7 +33,7 @@ if [ $? -eq 0 ]; then
 	do
 		if [[ "$FILE" == *.h264 ]]; then
 			ffmpeg -framerate 24 -i "$FILE" -c copy "${FILE%.*}.mp4"
-			ffmpeg -i "$FILE" -ss 00:00:01.000 -vframes 1 "${FILE%.*}.png"
+			ffmpeg -i "$FILE" -ss 00:00:10.000 -vframes 1 "${FILE%.*}.png"
 		fi
 	done
 	echo "Done conversion"
