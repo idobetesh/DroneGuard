5acc75c5babcdfd256203652dc8daf2b687b891c
yalisofer94 <yalisofer@gmail.com>
2022-05-03 13:19:24 +0300

    yali/changed lambda to move object to new bucket and delete the files

--- debriefing-service/lambdas/upload-debriefing-obj.py
+++ debriefing-service/lambdas/upload-debriefing-obj.py
@@ -1,35 +1,46 @@
-import json
-import requests
+import urllib.parse
+import boto3
+
+print('Loading function')
+
+s3 = boto3.resource('s3')
+
+
 
 def lambda_handler(event, context):
-    # Will change to deployed server url
-    URL="http://localhost:3001/api/user/login"
-    email = ''
-    password = ''
-    requestBody = {}
     
-    try:
-        print(event)
-        if event.get('body'):
-            requestBody = json.loads(event["body"])
-            
-        if requestBody.get('email') and requestBody.get('password'):
-            email = requestBody['email']
-            password = requestBody['password']
-        
-        if password and email:
-            res = requests.post(URL, body={'email': email, 'password': password})
-            print(res)
-        
-    except:
-        return {
-            'statusCode': 400,
-            'error': json.dumps('Error occurred')
+    # Get the object from the event and show its content type
+    src_bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    parsed_key = key.split(".")
+    second_key = ""
+    
+    if parsed_key[1] == "mp4":
+        second_key = parsed_key[0] + '.png'
+    elif parsed_key[1] == "png":
+        second_key = parsed_key[0] + '.mp4'
+    
+    copy_item1 = {
+            'Bucket': src_bucket,
+            'Key': key
         }
-    return {
-        'statusCode': 200,
-        'body': json.dumps({
-            "email": email,
-            "password": password
-        })
-    }
+    copy_item2 = {
+            'Bucket': src_bucket,
+            'Key': second_key
+        }
+        
+    try:
+        print(key, second_key)
+        # Copying both items to drone-guard-debriefing
+        s3.meta.client.copy(copy_item1, 'drone-guard-debriefing', key)
+        s3.meta.client.copy(copy_item2, 'drone-guard-debriefing', second_key)
+        
+        # Deleting both items from the mid bucket
+        s3.meta.client.delete_object(Bucket=src_bucket, Key=key)
+        s3.meta.client.delete_object(Bucket=src_bucket, Key=second_key)
+    
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, src_bucket))
+        raise e
+              
