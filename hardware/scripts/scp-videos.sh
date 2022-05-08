620be2bf5d87c78f9c8621779d95b85e2e667be1
idobetesh <idobetesh@gmail.com>
2022-05-08 19:51:26 +0300

    update script

--- hardware/scripts/scp-videos.sh
+++ hardware/scripts/scp-videos.sh
@@ -3,14 +3,12 @@
 # Secure copy videos from RP to our local machine
 
 # Path to the videos folder in RP
-SRC_DIR_NAME="pi@169.254.232.194:~/recordings"
+SRC_DIR_NAME="pi@169.254.232.194:'~/recordings/*.h264'"
 # Path to the videos folder in local machine
 DEST_DIR_NAME=/Users/ido/Desktop/local-videos/recordings
 
 # Check connection with RP
-ping -q -c 1 -w 1 raspberrypi.local >/dev/null 2>&1
-
-if [ $? -eq 0 ]; then
+if ping -q -c 1 -W 1 raspberrypi.local >/dev/null; then
     scp -r $SRC_DIR_NAME $DEST_DIR_NAME
     echo "Done"
 else
