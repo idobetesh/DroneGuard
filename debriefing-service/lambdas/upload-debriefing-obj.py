2098cf4a0206c86bd4e0e4f967bd9a3d1b95a326
yalisofer94 <61949478+yalisofer94@users.noreply.github.com>
2022-05-12 13:57:15 +0300

    Yali/update lambda function (#59)
    * update working lambda function
    
    * yali/clean lambda function

--- debriefing-service/lambdas/upload-debriefing-obj.py
+++ debriefing-service/lambdas/upload-debriefing-obj.py
@@ -1,21 +1,28 @@
 import urllib.parse
 import boto3
+import requests
 
 print('Loading function')
 
 s3 = boto3.resource('s3')
+exists = False
+
+base_debriefing_url = 'https://droneguard-debriefing-server.herokuapp.com/api'
+login_url = f"{base_debriefing_url}/user/login"
+records_url = f"{base_debriefing_url}/record"
+s3_bucket_url = "https://drone-guard-debriefing.s3.eu-west-1.amazonaws.com"
 
 def lambda_handler(event, context):
     
     # Get the object from the event and show its content type
     src_bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    parsed_key = key.split('.')
-    second_key = ''
+    parsed_key = key.split(".")
+    second_key = ""
     
-    if parsed_key[1] == 'mp4':
+    if parsed_key[1] == "mp4":
         second_key = parsed_key[0] + '.png'
-    elif parsed_key[1] == 'png':
+    elif parsed_key[1] == "png":
         second_key = parsed_key[0] + '.mp4'
     
     copy_item1 = {
@@ -28,7 +35,6 @@ def lambda_handler(event, context):
         }
         
     try:
-        print(key, second_key)
         # Copying both items to drone-guard-debriefing
         s3.meta.client.copy(copy_item1, 'drone-guard-debriefing', key)
         s3.meta.client.copy(copy_item2, 'drone-guard-debriefing', second_key)
@@ -36,6 +42,30 @@ def lambda_handler(event, context):
         # Deleting both items from the mid bucket
         s3.meta.client.delete_object(Bucket=src_bucket, Key=key)
         s3.meta.client.delete_object(Bucket=src_bucket, Key=second_key)
+        
+        # Log in as admin
+        login_body_req = {
+        "email": "email",
+        "password": "password"
+        }   
+        
+        login_res = requests.post(login_url, data=login_body_req)
+        if login_res.status_code == 200:
+            token = login_res.json()['token']
+            if token:
+                heds = {'Authorization': f"Bearer {token}"}
+                get_records_res = requests.get(records_url, headers=heds)
+                list_of_all_values = [value for elem in get_records_res.json()
+                      for value in elem.values()]
+                if key not in list_of_all_values:
+                    add_record_body = {
+                        "url": f"{s3_bucket_url}/{parsed_key[0]}.mp4",
+                        "thumbnailUrl": f"{s3_bucket_url}/{parsed_key[0]}.png"
+                    }
+                    add_record_res = requests.post(records_url, 
+                    data=add_record_body, 
+                    headers=heds)
+            
     
     except Exception as e:
         print(e)
