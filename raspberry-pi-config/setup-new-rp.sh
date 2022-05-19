fba2ec44dcdbef18917bb689ae7d5c668b1850aa
idobetesh <58806763+idobetesh@users.noreply.github.com>
2022-05-19 13:02:51 +0300

    Ido/update boot script (#61)
    * add install of vim and pip3
    
    * update readme insructions for setting up new RP

--- raspberry-pi-config/setup-new-rp.sh
+++ raspberry-pi-config/setup-new-rp.sh
@@ -13,7 +13,7 @@
 # BUG_REPORT_URL="http://www.raspbian.org/RaspbianBugs"
  
 echo === Update current OS === 
-sudo apt install ntp;
+sudo apt -y install ntp;
 sudo apt clean; sudo apt update; sudo apt list --upgradable;
 sudo apt dist-upgrade;
 sudo apt autoremove;   
@@ -26,6 +26,9 @@ source ~/.bash_aliases;
 echo === Install git ===
 sudo apt -y install git;
 
+echo === Install vim ===
+sudo apt -y install vim;
+
 echo === Install Node and NPM ===
 sudo apt -y install nodejs;
 sudo apt -y install npm;
@@ -33,6 +36,10 @@ sudo apt -y install npm;
 echo === Install Python ===
 sudo apt-get -y install python3;
 echo alias python=python3 >> ~/.bash_aliases; sleep 0.5; source ~/.bash_aliases;
+
+echo === Install pip ===
+sudo apt-get -y install python3-pip;
+echo alias pip=pip3 >> ~/.bash_aliases; sleep 0.5; source ~/.bash_aliases;
 
 echo === Set Local Time ===
 sudo dpkg-reconfigure tzdata;
